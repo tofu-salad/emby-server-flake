ac7fbc65783c4849534a924fdda5ba7a965d7551
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-08-24 03:37:17 -0300

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# Emby Media Server Nix Flake
+
+This Nix flake provides:
+
+- A wrapped Emby Media Server package using the official `.deb` release
+- A reusable NixOS module to enable Emby as a system service (`services.emby.enable = true`)
+
+> ⚠️ **Note:** Emby is proprietary software. This flake does **not** build from source, but unpacks and wraps the official `.deb` release.
+
+---
+
+## Usage
+
+### Add the flake as an input in your `flake.nix`
+
+```nix
+{
+  inputs.emby-flake.url = "github:tofu-salad/emby-server-flake";
+
+  outputs = { self, nixpkgs, emby-flake, ... }: {
+    nixosConfigurations.mysystem = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        emby-flake.nixosModules.default
+      ];
+    };
+  };
+}
+```
+### Enable Emby in your NixOS configuration (configuration.nix)
+```nix
+{
+  services.emby.enable = true;
+  # Optional overrides:
+  # services.emby.user = "emby";
+  # services.emby.group = "emby";
+  # services.emby.dataDir = "/var/lib/emby/ProgramData-Server";
+}
+```
+### Build or run the wrapped Emby package directly
+```nix
+nix build github:youruser/emby-flake
+./result/bin/emby
+```
+### Or run directly with:
+```nix
+nix run github:youruser/emby-flake
+```
+# Credits
+Big thanks to [@numkem](https://emby.media/community/index.php?/topic/109786-live-tv-broken-on-47x/#comment-116375) from the emby.media community forum.
+NixOS maintainers for the old official Emby service module available [here](https://github.com/NixOS/nixpkgs/blob/ebbeae56bdce0226ca66414160345ccdf457284b/nixos/modules/services/misc/emby.nix).
+  https://github.com/tofu-salad/emby-server-flake
+
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Emby Media Server flake package with NixOS module";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+      emby = pkgs.stdenv.mkDerivation rec {
+        pname = "emby-server";
+        version = "4.8.11.0";
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/MediaBrowser/Emby.Releases/releases/download/${version}/${pname}-deb_${version}_amd64.deb";
+          sha256 = "sha256-Zf3Klu6sQysbG9i8W64gknTG3+suLDS0GpaVfyU5TEA=";
+        };
+
+        buildInputs = [
+          pkgs.dpkg
+          pkgs.lttng-ust_2_12
+        ];
+
+        nativeBuildInputs = [
+          pkgs.autoPatchelfHook
+          pkgs.makeWrapper
+        ];
+
+        unpackPhase = "dpkg-deb -x $src $out";
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp -r * "$out/opt/emby-server"
+          rm -rf $out/opt/emby-server/lib/systemd
+          rm -rf $out/opt/emby-server/licenses
+
+          sed -i "s|/opt|$out/opt|g" $out/opt/emby-server/bin/*
+
+          makeWrapper "$out/opt/emby-server/bin/emby-server" $out/bin/emby \
+            --prefix LD_LIBRARY_PATH : "$out/opt/emby-server/lib" \
+            --add-flags "$out/opt/emby-server/EmbyServer.dll -ffmpeg $out/opt/emby-server/bin/emby-ffmpeg -ffprobe $out/opt/emby-server/bin/emby-ffprobe"
+
+          makeWrapper "$out/opt/emby-server/bin/emby-ffmpeg" $out/bin/emby-ffmpeg \
+            --prefix LD_LIBRARY_PATH : "$out/opt/emby-server/lib"
+
+          makeWrapper "$out/opt/emby-server/bin/emby-ffdetect" $out/bin/emby-ffdetect \
+            --prefix LD_LIBRARY_PATH : "$out/opt/emby-server/lib"
+        '';
+      };
+    in
+    {
+      packages.${system}.default = emby-server;
+      nixosModules.default = import ./module.nix;
+    };
+}
